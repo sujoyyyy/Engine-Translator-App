73eb9cfd6fb1878ce12d6bcc308699a295a13a8b
Sujoy Datta <sujoydatta.2009@gmail.com>
2021-01-12 23:25:40 +0530

    Gradle changes for the app to run on android studio latest version.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,12 +18,24 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-        String[] words= {"zero","one","two","three","four","five","six","seven","eight","nine"};
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("zero");
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
     }
 }
